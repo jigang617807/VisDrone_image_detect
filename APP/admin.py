18d7bde0affa8f967ce3e8251642f5e27385fb78
jigang617807 <91206596+jigang617807@users.noreply.github.com>
2025-04-09 17:12:19 +0800

    Delete APP directory

--- APP/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from App.models import Student, UserModel, Recycle_Bin
-
-admin.site.register([Student, UserModel, Recycle_Bin])
--- APP/utils.py
+++
@@ -1,8 +0,0 @@
-# 上面生成的验证码不是随机的，是我们自己制定的，下面定义一个函数来获取随机串
-import random
-def generatr_code():
-    source='78915463ASXZEGSLPkl105454dasdwauhuihjSDASXAIUOEJAJNSIDJADASCVKLPOPOSDnaixniauhiufias12R4'
-    code = ""
-    for i in range(4):
-        code+=random.choice(source)
-    return code
